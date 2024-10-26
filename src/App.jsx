0af03cecaf1cdff5d5d1705a9898945f6b33f6f8
Midhun P J <midhunjayan.official@gmail.com>
2024-10-26 15:37:18 +0530

    feat: hero section added for portfolio

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  )
+}
+
+export default App
